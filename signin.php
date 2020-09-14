98022885293193cc5fe3cb5d220637c2667dba4f
tjschwartz7 <54190751+tjschwartz7@users.noreply.github.com>
2020-09-14 19:53:48 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -17,12 +17,6 @@ require 'requires/nav2.php';
     <body>
 
       <form id="loginbox" action="include/signin.inc.php" method="post">
-  <!--  <div class="loginimg-container">
-      <div class="loginimg-item loginimg-item-1">
-      <h1 id="siteName">Nerdy Owl</h1>
-    </div>
-    </div>
-  -->
 
     <div class="login-container">
       <ul>
@@ -32,17 +26,6 @@ require 'requires/nav2.php';
   </li>
 
   <li>
-  <!--  <script type="text/javascript">
-    document.addEventListener('DOMContentLoaded',
-    function(){
-      document.getElementById('submit').onclick = function(){
-    var pw1 = document.getElementsByClassName("pw1").value;
-    var pw2 = document.getElementsByClassName("pw2").value;
-    if(pw1 !== pw2) document.getElementsByClassName("passwordlabel")[0].textContent = "Passwords do not match";
-    else document.getElementsByClassName("passwordlabel")[0].textContent= "Passwords match!";
-  };
-  });
-  </script>-->
       <label for="pw1"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="pw1" class="pw1" required>
     </li>
@@ -61,7 +44,7 @@ require 'requires/nav2.php';
   <a href="signup.php">  <button type="button">Create New Account</button></a>
   </li>
     <li>
-      <span class="pw">Forgot <a href="#">password?</a></span>
+      <span class="pw">Forgot <a href=" ">password?</a></span>
     </li>
   </ul>
   <label>
